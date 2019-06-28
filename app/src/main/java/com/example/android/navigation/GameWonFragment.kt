54995770ab3102b41de7b0c18a62fed01e091e49
Dillon McDowell <djmcdowell81@gmail.com>
2019-06-28 08:01:48 -0700

    finished Trivia App

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -25,6 +25,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
+import android.content.pm.ResolveInfo
+import android.content.pm.PackageManager
 
 
 class GameWonFragment : Fragment() {
@@ -41,17 +43,28 @@ class GameWonFragment : Fragment() {
         return binding.root
     }
 
+
+
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        return ShareCompat.IntentBuilder.from(activity)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu, menu)
-    }
-
-    private fun getShareIntent() : Intent {
-        var args = GameWonFragmentArgs.fromBundle(arguments!!)
-        //val shareIntent = Intent(Intent.ACTION_SEND)
-        return ShareCompat.IntentBuilder.from(activity)
-                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
-                .intent
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -60,10 +73,4 @@ class GameWonFragment : Fragment() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    private fun shareSuccess() {
-        startActivity(getShareIntent())
-    }
-
-
 }
--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -20,9 +20,8 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding: FragmentTitleBinding =
-                DataBindingUtil.inflate(inflater, R.layout.fragment_title, container,
-                        false)
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+	inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener { v: View ->
             v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
